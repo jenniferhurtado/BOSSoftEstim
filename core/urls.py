b4cfb990115544f1d9dee32f18c60518a05a66d8
Jennifer Hurtado <jenniferhurtadopareja@gmail.com>
2019-05-21 20:20:07 +0200

    Add root to urls

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('jira.urls')),
     path('jira/', include('jira.urls')),
     path('admin/', admin.site.urls),
 ]
